b64ff882035b4c8b0f2000689dadcdd6b3a101e1
SandraCarretero <sandracarretero24@gmail.com>
2024-02-29 17:28:48 +0100

    start porfolio

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const backgroundElement = document.getElementById('background');
+
+
+const moveBackground = (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    const halfWidth = window.innerWidth / 2;
+    const halfHeight = window.innerHeight / 2;
+
+    const backgroundX = mouseX - halfWidth;
+    const backgroundY = mouseY - halfHeight;
+
+    backgroundElement.style.backgroundPosition = `${backgroundX}px ${backgroundY}px`;
+};
+
+document.addEventListener('mousemove', moveBackground);
